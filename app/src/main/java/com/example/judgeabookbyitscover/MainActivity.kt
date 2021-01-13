19da6211d2b7c3e47b67c3cd94bce1c057895928
lilymtang <lilyytang@gmail.com>
2021-01-12 20:27:32 -0800

    wip: add system notification

--- app/src/main/java/com/example/judgeabookbyitscover/MainActivity.kt
+++ app/src/main/java/com/example/judgeabookbyitscover/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.judgeabookbyitscover
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import com.example.judgeabookbyitscover.MyAlarmReceiver.Companion.NOTIF_CHANNEL_ID
+import com.example.judgeabookbyitscover.MyAlarmReceiver.Companion.calendar
 import com.example.judgeabookbyitscover.model.db.BookDatabase
 import com.example.judgeabookbyitscover.model.db.Repository
 import com.example.judgeabookbyitscover.ui.main.SectionsPagerAdapter
 import com.google.android.material.tabs.TabLayout
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,25 +36,39 @@ class MainActivity : AppCompatActivity() {
         db = BookDatabase.getInstance(applicationContext)
         repository = Repository(db)
 
-        scheduleAlarm();
+        createNotificationChannel()
+        scheduleAlarm()
     }
 
-    // Setup a recurring alarm every half hour
-    fun scheduleAlarm() {
-        // Construct an intent that will execute the AlarmReceiver
+    // Set up alarm
+    private fun scheduleAlarm() {
+        // This intent that will execute the AlarmReceiver
         val intent = Intent(applicationContext, MyAlarmReceiver::class.java)
 
-        // Create a PendingIntent to be triggered when the alarm goes off
-        val pIntent = PendingIntent.getBroadcast(this, MyAlarmReceiver.REQUEST_CODE,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        // Setup periodic alarm every every half hour from this point onwards
+        // Set up periodic alarm every every half hour from this point onwards
         val firstMillis = System.currentTimeMillis() // alarm is set right away
         val alarm = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
-        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
-                60 * 1, pIntent)
+        // PendingIntent will be triggered when the alarm goes off
+        val pIntent = PendingIntent.getBroadcast(this, MyAlarmReceiver.REQUEST_CODE,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis,AlarmManager.INTERVAL_DAY * 7, pIntent)
+//        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,60, pIntent)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(NOTIF_CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/com/example/judgeabookbyitscover/NotificationService.kt
+++ app/src/main/java/com/example/judgeabookbyitscover/NotificationService.kt
@@ -1,16 +1,45 @@
 package com.example.judgeabookbyitscover
 
+import android.app.PendingIntent
 import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
-import android.widget.Toast
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.judgeabookbyitscover.MyAlarmReceiver.Companion.NOTIF_CHANNEL_ID
+import java.util.*
 
 class NotificationService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show()
+
+        // Create intent to open the app when notification is tapped
+        val openAppIntent = Intent(this, MainActivity::class.java)
+                .apply { Intent.FLAG_ACTIVITY_CLEAR_TASK }
+
+        var builder = NotificationCompat.Builder(this, NOTIF_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_library_books_24)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_text))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(PendingIntent.getActivity(this, 0, openAppIntent, 0))
+                .setAutoCancel(true)
+
+        // Show notification
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, builder.build())
+        }
+
         return START_STICKY
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        val intent = Intent("com.android.ServiceStopped")
+        sendBroadcast(intent)
     }
 
     // Default implementation returns  null - no need to implement
@@ -22,13 +51,27 @@ class NotificationService : Service() {
 class MyAlarmReceiver : BroadcastReceiver() {
     // Triggered by the Alarm periodically (starts the service to run task)
     override fun onReceive(context: Context, intent: Intent?) {
+        Log.d("TAG", "Starting service")
         val i = Intent(context, NotificationService::class.java)
-        i.putExtra("foo", "bar")
         context.startService(i)
     }
 
     companion object {
-        const val REQUEST_CODE = 12345
-        const val ACTION = "com.codepath.example.servicesdemo.alarm"
+        const val NOTIF_CHANNEL_ID = "default"
+        const val REQUEST_CODE = 100001
+
+        // Wednesdays at 7 PM EST
+        lateinit var calendar: Calendar
+
+        init {
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("EST"))
+            calendar[Calendar.DAY_OF_WEEK] = 4
+            // Check we aren't setting it in the past which would trigger it to fire instantly
+            if (Companion.calendar.timeInMillis < System.currentTimeMillis()) {
+                Companion.calendar.add(Calendar.DAY_OF_YEAR, 7)
+            }
+            calendar.set(Calendar.HOUR, 7)
+            calendar.set(Calendar.AM_PM, Calendar.PM)
+        }
     }
 }

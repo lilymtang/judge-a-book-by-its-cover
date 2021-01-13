2e0dbdda06f60979285b9746f6c4dbefb739f43f
lilymtang <lilyytang@gmail.com>
2021-01-13 07:51:23 -0800

    refactor: deprecate files

--- app/src/main/java/com/example/judgeabookbyitscover/model/datamodels/Volume.kt
+++
@@ -1,7 +0,0 @@
-package com.example.judgeabookbyitscover.model.datamodels
-
-data class Volume(
-    val id: String,
-    val selfLink: String,
-    val volumeInfo: VolumeInfo
-)
--- app/src/main/java/com/example/judgeabookbyitscover/model/db/Result.kt
+++
@@ -1,8 +0,0 @@
-package com.example.judgeabookbyitscover.model.db
-
-sealed class Result<out T : Any> {
-
-    class Success<out T : Any>(val data: T) : Result<T>()
-
-    class Error(val exception: Throwable, val message: String = exception.localizedMessage) : Result<Nothing>()
-}
